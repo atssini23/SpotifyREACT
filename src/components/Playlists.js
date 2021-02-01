f3bbe3f9525c546638e066288cfccdc529e9320c
Atssini Cabrera <atssini23@yahoo.com>
2021-02-01 15:21:45 -0800

    playIcon displayed updated playIcon: now appears when you hover over it!!

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -74,7 +74,20 @@ const Playlists = (props) => {
             <h3>{playlist.name}</h3>
             <span>{playlist.des}</span>
           </div>
-          <span className="playIcon">></span>
+          <span className="playIcon">
+            <svg
+              height="16"
+              role="img"
+              width="16"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <polygon
+                points="21.57 12 5.98 3 5.98 21 21.57 12"
+                fill="currentColor"
+              ></polygon>
+            </svg>
+          </span>
         </div>
       ))}
     </div>
