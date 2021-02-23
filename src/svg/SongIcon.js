a5f54337e861cabb85f0c5e3c9e3f5dbcf9670a6
Atssini Cabrera <atssini23@yahoo.com>
2021-02-23 11:54:35 -0800

    svg file added for icons

---
+++ src/svg/SongIcon.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SongIcon = () => {
+  return (
+    <svg
+      height="32"
+      role="img"
+      width="32"
+      viewBox="0 0 24 24"
+      class="_61eb6013554e346ba715319eaf489008-scss"
+    >
+      <polygon
+        points="21.57 12 5.98 3 5.98 21 21.57 12"
+        fill="currentColor"
+      ></polygon>
+    </svg>
+  );
+};
+export default SongIcon;
